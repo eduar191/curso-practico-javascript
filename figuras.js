a7f6acb0a8cca36e5dbea2ad869026021353066c
eduar <eduarhdezglez@hotmail.com>
2022-06-22 19:53:34 -0500

    Actualizacion de archivos

---
+++ figuras.js
@@ -0,0 +1,39 @@
+// Código del cuadrado
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm^2");
+console.groupEnd();
+
+// Código del triangulo
+console.group("Triangulos")
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "Los lados del Triangulo miden: " 
+    + ladoTriangulo1 
+    + "cm,"  
+    + ladoTriangulo2 
+    + "cm," 
+    + baseTriangulo 
+    + "cm"
+);
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triángulo es de: " + alturaTriangulo + "cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perímetro del triángulo es: " + perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triángulo es: " + areaTriangulo + "cm^2");
+
+
+console.groupEnd();
